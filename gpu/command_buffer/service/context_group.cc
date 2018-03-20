7d227a7229430b70533798a3e4ec974c4ddfa980
Sammy Yu <syu@brightedge.com>
2018-03-20 09:15:45 -0700

    Disable blacklist opengl vendor feature call

--- gpu/command_buffer/service/context_group.cc
+++ gpu/command_buffer/service/context_group.cc
@@ -130,6 +130,7 @@ gpu::ContextResult ContextGroup::Initialize(
     DecoderContext* decoder,
     ContextType context_type,
     const DisallowedFeatures& disallowed_features) {
+/*
   switch (context_type) {
     case CONTEXT_TYPE_WEBGL1:
       if (kGpuFeatureStatusBlacklisted ==
@@ -149,6 +150,7 @@ gpu::ContextResult ContextGroup::Initialize(
     default:
       break;
   }
+*/
   if (HaveContexts()) {
     if (context_type != feature_info_->context_type()) {
       LOG(ERROR) << "ContextResult::kFatalFailure: the type of "
